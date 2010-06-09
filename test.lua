3311789e1a4d11e42f86fe5b90a04b902a6e655f
Michal Kottman <michal.kottman@gmail.com>
2010-06-09 12:54:49 +0200

    added a test

---
+++ test.lua
@@ -0,0 +1,43 @@
+require 'spell'
+
+assert(spell, 'spell not loaded')
+assert(type(spell) == "function", 'spell is not a function')
+
+s = spell('/usr/share/hunspell/en_US.aff', '/usr/share/hunspell/en_US.dic')
+assert(s, 'dict not loaded')
+
+local okword = 'friendliness'
+local badword = 'friendlines'
+
+assert(s.spell, 'spell function does not exist')
+assert(s:spell(okword), 'spell("'..okword..'") should return true')
+assert(not s:spell(badword), 'spell("'..badword..'") should return false')
+
+assert(s.suggest, 'suggest function does not exist')
+local t = s:suggest(badword)
+local found = false
+for _,v in ipairs(t) do
+	if v == okword then
+		found = true
+		break
+	end
+end
+assert(found, badword .. ' -> '..okword..' suggestion not found')
+
+assert(s.analyze, 'analyze function does not exist')
+local t = s:analyze(okword)
+assert(type(t) == "table", 'analyze() does not return a table: '..type(t))
+print('Analysis of '..okword)
+for _,v in ipairs(t) do
+	print('>', v)
+end
+
+
+local root = s:stem(okword)
+assert(type(root) == "table", 'analyze() does not return a table: '..type(root))
+print('Roots of '..okword)
+for _,v in ipairs(root) do
+	print('>', v)
+end
+
+print('OK')
